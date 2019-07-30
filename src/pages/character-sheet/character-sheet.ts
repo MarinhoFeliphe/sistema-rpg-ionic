907e0e6ee5c6edfc6e827af976161b9d8b56512f
Feliphe Marinho <feliphemarinho18@gmail.com>
2019-07-30 17:02:19 -0300

    Tela CharacterSheet iniciada

---
+++ src/pages/character-sheet/character-sheet.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-character-sheet',
+  templateUrl: 'character-sheet.html',
+})
+export class CharacterSheetPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CharacterSheetPage');
+  }
+
+}
